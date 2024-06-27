698053ac0021c4b524b27ebe9387c93e51a068ca
Henry645 <racamier.henry@gmail.com>
2024-06-27 20:02:51 +0800

    Separated js code into new file

---
+++ myscripts.js
@@ -0,0 +1,61 @@
+
+// const name = "My name is";
+// const user = "Henry";        
+// alert(name + ": " + user + ".");
+// alert("Roses are red\nViolets are blue.")
+// let digits = 314;
+// let digits_string = String(digits);
+// let digits_length = digits_string.length;
+// console.log(typeof(digits));
+// console.log(typeof(digits_string));
+// console.log(digits_length);
+// let sample = "madness";
+// let character = sample.charAt(3);
+// let character_code = sample.charCodeAt(3);
+// let character_new = sample.at(3);
+// let character_position = sample[3];
+// console.log(`Specific character(s): ${character}.`);
+// console.log(`Specific character code: ${character_code}.`);
+// console.log(`Specific character(s): ${character_new}.`);
+// console.log(`Specific character(s): ${character_position}.`);
+// alert(1/0);
+// alert("test"/2);
+// console.log(9007199254740991 + 1); 
+// console.log(9007199254740991 + 2); 
+// const bigInt = 1234567890123456789012345678901234567890n;
+// alert(bigInt);
+// let string = "needs single or double quotes, or backtick"
+// alert(`backtick allows for other variables or expressions: 2 + 5 = ${2+5}`)
+// let isGreater = 4 > 1;
+// alert(`4 > 1 = ${isGreater}` ); true (the comparison result is "yes")
+// let randomVar = null;
+// alert(randomVar);
+// let und;
+// alert(und);
+// const MAX = 57;
+// let actual = MAX - 13;
+// percentage = actual / MAX;
+// let a = 10;
+// console.log(a);
+// let b = 7*a
+// console.log(b)
+// const myInt = 5;
+// const myFloat = 6.667;
+// alert(typeof myInt);
+// alert(typeof myFloat);
+// const decimalGalore = 45.85924848398439924
+// alert(decimalGalore);
+// const twoDecimals = decimalGalore.toFixed(2)
+// alert(60%3);
+// let x = 7;
+// x += 2;
+// alert(x);
+// let myNumber = "74";
+// myNumber = Number(myNumber) + 3;
+// let a = prompt("First number?", 1);
+// let b = prompt("Second number?", 2);
+
+// alert(+a + +b); // 12
+// alert(myNumber);
+// console.log(23+45)
+// alert((4 + 6 + 9) / 77);
